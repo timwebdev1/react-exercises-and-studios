d437a5e7f37d33cec79cd7cbe9918b81507d7c87
Sally Steuterman <sally.steuterman@gmail.com>
2023-10-19 16:09:39 -0500

    Updating solutions to exercises

--- part2/exercises-2/src/App.jsx
+++ part2/exercises-2/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
 import Banner from './components/Banner';
-import Button from './components/Button';
 
 function App(){
   return(
     <>
       <Banner />
       <Profile />
-      <Button />
     </>
   );
 }
